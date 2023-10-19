52616f2bcf4edc1a710078188dcbcc2bdceede3f
Samson Iweibo <135668840+SamsonI95@users.noreply.github.com>
2023-10-19 02:25:05 +0100

    Add files via upload

---
+++ demo/signin.js
@@ -0,0 +1,25 @@
+document.getElementById("openSignup").addEventListener("click", function() {
+    var overlay = document.getElementById("overlay");
+    var popup = document.getElementById("popup");
+    overlay.style.display = "block";
+    popup.style.display = "block";
+});
+
+document.getElementById("closeButton").addEventListener("click", function() {
+    var overlay = document.getElementById("overlay");
+    var popup = document.getElementById("popup");
+    overlay.style.display = "none";
+    popup.style.display = "none";
+});
+
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    // Prevent the form from submitting and refreshing the page
+    event.preventDefault();
+    var overlay = document.getElementById("overlay");
+    var popup = document.getElementById("popup");
+    overlay.style.display = "none";
+    popup.style.display = "none";
+
+    // Navigate to the main page (index.html in this case)
+    window.location.href = "index.html";
+});
